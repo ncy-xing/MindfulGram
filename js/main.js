7500649a59ed13d59806c77ec9dfd89cba3d5966
‘Roxana <rvaldez@bowdoin.edu>
2023-04-04 15:45:37 -0400

    Adding like button functionality

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const btn = document.querySelector('.btn');
+let isRed = false; //like button starts (unliked) gray
+
+if (btn) {
+    btn.addEventListener('click', likeButton);
+  }
+  
+function likeButton(){
+    if(!isRed){
+        btn.style.color = "red";
+        isRed = true;
+    }
+    else{ 
+        btn.style.color = "lightgray";
+        isRed = false;
+    }
+}
+
+// function clickButton(){
+//     var element = document.getElementById(btn);
+
+//     if (count == 0) {
+//         element.style.color = "red"
+//         clickCounter = 1;        
+//     }
+//     else {
+//         element.style.color = "lightgray"
+//         clickCounter = 0;
+//     }
+   
+// }
+
+// function likeButton(){
+//     btn.classList.toggle("liked")
+// }
+
