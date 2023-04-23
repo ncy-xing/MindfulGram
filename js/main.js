a83319b2eb2b36ab0db690c88483b21125e4e1b5
Nancy Xing <92757073+ncy-xing@users.noreply.github.com>
2023-04-23 09:58:18 -0400

    list posts in js

--- js/main.js
+++ js/main.js
@@ -1,50 +1,55 @@
-
-// const btn = document.querySelector('.btn');
-// let isRed = false; //like button starts (unliked) gray
-
-// btn.addEventListener('click', clickButton())
-
-// function clickButton(){
-//     if(!isRed){
-//         btn.classList.toggle('liked');
-//         isRed = true;
-        
-//     } else {
-//         btn.classList.toggle('liked');
-//         isRed = false;
-//     }
-// }
-
-// var img = new Image();
-// var div = document.getElementById('test');
-// img.src = 'assets/image.jpg';
-
-// img.onload = function() {
-//   div.appendChild(img);
-// };
-
-let data = ["image", "image-2"];
-data.forEach(loadPost);
-
-function loadPost(value){
-    console.log(value);
-    var div = document.createElement("img"); 
-    div.setAttribute("src", "assets/" + value + ".jpg");
-    div.setAttribute("class", "blog-image");
-    document.body.appendChild(div);
-}
-
 /**
  * Post's Attributes 
  */
 
-const instaPost = {
+const postData = {
     id: 1,
-    pfName: "",
-    pfImage: "",
-    postTimeStamp: 0,
-    postImage: "",
+    username: "Friendly_Flower",
+    pfImage: "pfp",
+    timeStamp: "1 day ago",
+    image: "image",
     //likeButtonState: false,
-    postComment: "",
+    comment: "These are nice ducks.",
 
 }
+
+
+function loadPost(postData){
+    // post container
+    var post = document.createElement("div"); 
+    post.setAttribute("class", "blog-post");
+    var meta = document.createElement("div");
+    meta.setAttribute("class", "blog-post-meta"); 
+
+    // Metadata
+    var pfp = document.createElement("img");
+    pfp.setAttribute("class", "profile-image");
+    pfp.setAttribute("src", "assets/" + postData.pfImage + ".jpg");
+
+    var username = document.createElement("span");
+    username.setAttribute("class", "blog-post-username");
+    username.appendChild(document.createTextNode(postData.username));
+
+    var timeStamp = document.createElement("span");
+    timeStamp.setAttribute("class", "blog-post-timestamp");
+    timeStamp.appendChild(document.createTextNode(" · ".concat(postData.timeStamp)));
+
+    // image and caption
+    var img = document.createElement("img"); 
+    img.setAttribute("src", "assets/" + postData.image + ".jpg");
+    img.setAttribute("class", "blog-image");
+    var caption = document.createElement("p");
+    caption.setAttribute("class", "blog-post-text");
+    caption.appendChild(document.createTextNode(postData.comment));
+
+    // Write HTML
+    var postItems = [pfp, username, timeStamp, img, caption];
+    postItems.forEach(element => {
+        meta.appendChild(element);
+    });
+    post.appendChild(meta);
+    document.getElementById("post-body").appendChild(post);
+
+}
+
+loadPost(postData);
